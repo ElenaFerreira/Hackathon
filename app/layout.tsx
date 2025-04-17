e67764a261f37a7ebcbd5dbe21a088b0720dba13
DumbWays31 <elena.ferreira@free.fr>
2025-04-17 18:11:38 +0200

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#10b981" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Re_pair" />
+        {/* <link rel="manifest" href="/images/favicon/site.webmanifest" /> */}
       </head>
       <body className="bg-gray-50 flex flex-col min-h-screen">
         <Header />
